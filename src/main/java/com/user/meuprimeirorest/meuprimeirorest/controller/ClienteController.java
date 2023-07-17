375ab91f4d71e82179e916619426159cfbccefb1
css <cauefc12@gmail.com>
2023-07-17 19:07:14 -0300

    Creating multiple parameters and Cliente's DTO

---
+++ src/main/java/com/user/meuprimeirorest/meuprimeirorest/controller/ClienteController.java
@@ -0,0 +1,20 @@
+package com.user.meuprimeirorest.meuprimeirorest.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.user.meuprimeirorest.meuprimeirorest.dto.Cliente;
+
+@RestController
+public class ClienteController {
+	
+	@PostMapping(path = "clientes/novo", consumes = "application/json")
+	public void novoCliente(@RequestBody Cliente cliente) {
+		
+		System.out.println(
+				"Nome do cliente: " + cliente.getNome()
+				+ " " + cliente.getSobrenome() + " | " + "Idade do cliente: " +
+						cliente.getIdade());
+	}
+}
